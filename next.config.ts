deafb191735fddfee83942d41b7746b0a4e6c497
Maycon Borges <mayconborgesgato717@gmail.com>
2025-07-24 16:53:53 -0300

    correction

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { NextConfig } from 'next'
 
 const isProd = process.env.NODE_ENV === 'production'
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   basePath: isProd ? '/dashboard-analytics' : '',
   assetPrefix: isProd ? '/dashboard-analytics/' : '',
   output: 'export',
